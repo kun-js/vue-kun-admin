f45bbd06364c3343c80f6a8c42a86f49d28e9b0d
zhyhjohn <879965956@qq.com>
2024-03-28 14:57:10 +0800

    feat: 新增异常页页面

---
+++ src/views/Pagedemo/Exception/components/fourzerothree.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="icon"><img class="img" src="@/assets/imgs/403.png" alt="" /></div>
+    <div class="text">403</div>
+    <div class="tip-text">抱歉,你无权访问页面!</div>
+    <div class="action"><el-button type="primary">返回首页</el-button></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 800px;
+  margin-top: 100px;
+
+  .icon {
+    .img {
+      width: 250px;
+      height: 200px;
+    }
+  }
+
+  .text {
+    margin-top: 12px;
+    font-size: 24px;
+  }
+
+  .tip-text {
+    margin-top: 12px;
+    font-size: 14px;
+    color: #8c8c8c;
+  }
+
+  .action {
+    margin-top: 12px;
+  }
+}
+</style>
---
+++ src/views/Pagedemo/Exception/components/networkerror.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="icon"><img class="img" src="@/assets/imgs/networkerror.png" alt="" /></div>
+    <div class="text">网络错误</div>
+    <div class="tip-text">抱歉,你的网络已断开,请检查你的网络!</div>
+    <div class="action"><el-button type="primary">刷新</el-button></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 800px;
+  margin-top: 100px;
+
+  .icon {
+    .img {
+      width: 250px;
+      height: 200px;
+    }
+  }
+
+  .text {
+    margin-top: 12px;
+    font-size: 24px;
+  }
+
+  .tip-text {
+    margin-top: 12px;
+    font-size: 14px;
+    color: #8c8c8c;
+  }
+
+  .action {
+    margin-top: 12px;
+  }
+}
+</style>
