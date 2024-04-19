3c8caf015621f6c34a8fe91e66b0cfd9cde31b37
zhyhjohn <84012676+zhyhjohn@users.noreply.github.com>
2024-04-19 17:06:35 +0800

    删除src文件夹

--- src/mockProdServer.ts
+++
@@ -1,9 +0,0 @@
-import { createProdMockServer } from "vite-plugin-mock/es/createProdMockServer";
-
-// 逐一导入您的mock.js文件
-// 如果使用vite.mock.config.js，只需直接导入文件
-// 可以使用 import.meta.glob功能来进行全部导入
-import index from "../mock/index";
-export function setupProdMockServer() {
-  createProdMockServer([...index]);
-}
