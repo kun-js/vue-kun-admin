df315fb909195ed0ac00914af6010a5fd389ec97
zhyhjohn <879965956@qq.com>
2024-04-22 09:23:45 +0800

    fix: 修复文件错误

--- src/views/external-page/iframe/vue/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <el-card style="max-width: 100%; height: calc(100vh - 48px)" :body-style="{ height: '100%', padding: 0 }">
-    <iframe src="https://cn.vuejs.org/" width="100%" height="100%"></iframe>
-  </el-card>
-</template>
-
-<script setup lang="ts"></script>
-
-<style lang="scss" scoped></style>
