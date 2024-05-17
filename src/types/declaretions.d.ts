68ded67c581f0e3749fa302f9c06b1efff5c2ab0
zhyhjohn <879965956@qq.com>
2024-05-17 14:20:40 +0800

    feat: 思维导图功能更新

--- src/types/declaretions.d.ts
+++ src/types/declaretions.d.ts
@@ -4,4 +4,3 @@ declare module "json-editor-vue3";
 declare module "vue-esign";
 declare module "element-plus/dist/locale/zh-cn.mjs";
 declare module "element-plus/dist/locale/en.mjs";
-declare module "simple-mind-map/src/plugins/AssociativeLine.js";
--- src/views/feature/mind-map/index.vue
+++ src/views/feature/mind-map/index.vue
@@ -1,188 +1,19 @@
 <template>
   <div class="mind-map-feature-container">
-    <el-card style="max-width: 100%; height: var(--card-height)" :body-style="{ height: '93%', padding: 0 }">
-      <template #header>
-        <span style="margin-right: 18px">思维导图示例</span>
-      </template>
-      <div class="mind-map-container">
-        <div
-          id="mindMapContainer"
-          style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; padding: 0; margin: 0"
-        ></div>
-        <div class="toolbar">
-          <el-button @click="back" v-if="!isStart">回退</el-button>
-          <el-button @click="forward" v-if="!isEnd">前进</el-button>
-          <el-button @click="insertNode" v-if="activeNodes.length > 0">插入兄弟节点</el-button>
-          <el-button @click="insertChildNode" v-if="activeNodes.length > 0">插入子节点</el-button>
-          <el-button @click="deleteNode" v-if="activeNodes.length > 0">删除节点</el-button>
-          <el-button @click="insertImage" :disabled="activeNodes.length <= 0">插入图片</el-button>
-          <el-button @click="insertIcon" :disabled="activeNodes.length <= 0">插入图标</el-button>
-          <el-button @click="insertLink" :disabled="activeNodes.length <= 0">插入超链接</el-button>
-          <el-button @click="insertNote" :disabled="activeNodes.length <= 0">插入备注</el-button>
-          <el-button @click="insertTag" :disabled="activeNodes.length <= 0">插入标签</el-button>
-          <el-button @click="insertGa" :disabled="activeNodes.length <= 0">插入概要</el-button>
-          <el-button @click="insertLine" :disabled="activeNodes.length <= 0">插入关联线</el-button>
-        </div>
-      </div>
+    <el-card style="max-width: 100%; height: calc(100vh - 48px)" :body-style="{ height: '100%', padding: 0 }">
+      <iframe src="https://wanglin2.github.io/mind-map/#/" width="100%" height="100%"></iframe>
     </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, shallowRef } from "vue";
-import MindMap from "simple-mind-map";
-import AssociativeLine from "simple-mind-map/src/plugins/AssociativeLine.js";
-
-MindMap.usePlugin(AssociativeLine);
-
 defineOptions({
   name: "MindMap",
-});
-
-let mindMap: MindMap | null = null;
-
-// 记录前进回退
-const isStart = ref(true);
-const isEnd = ref(true);
-
-// 当前激活的节点列表
-const activeNodes = shallowRef([]);
-
-// 回退
-const back = () => {
-  mindMap.execCommand("BACK");
-};
-
-// 前进
-const forward = () => {
-  mindMap.execCommand("FORWARD");
-};
-
-// 插入兄弟节点
-const insertNode = () => {
-  mindMap.execCommand("INSERT_NODE");
-};
-
-// 插入子节点
-const insertChildNode = () => {
-  mindMap.execCommand("INSERT_CHILD_NODE");
-};
-
-// 删除节点
-const deleteNode = () => {
-  mindMap.execCommand("REMOVE_NODE");
-};
-
-// 插入图片
-const insertImage = () => {
-  activeNodes.value.forEach((node) => {
-    node.setImage({
-      url: "https://lxqnsys.oss-cn-beijing.aliyuncs.com/qlx/xh2AXkBxYm5jGe5fD7DWYrC5b.png",
-      title: "图片的标题或描述",
-      width: 100, // 图片的宽高也不能少
-      height: 100,
-    });
-  });
-};
-
-// 插入图标
-const insertIcon = () => {
-  const iconList = ["priority_1", "priority_2"];
-  activeNodes.value.forEach((node) => {
-    node.setIcon(iconList);
-  });
-};
-
-// 插入超链接
-const insertLink = () => {
-  activeNodes.value.forEach((node) => {
-    node.setHyperlink("http://lxqnsys.com/", "理想青年实验室");
-  });
-};
-
-// 插入备注
-const insertNote = () => {
-  activeNodes.value.forEach((node) => {
-    node.setNote("备注内容");
-  });
-};
-
-// 插入标签
-const insertTag = () => {
-  activeNodes.value.forEach((node) => {
-    node.setTag(["标签1", "标签2"]);
-  });
-};
-
-// 插入概要
-const insertGa = () => {
-  mindMap.execCommand("ADD_GENERALIZATION", {
-    text: "自定义概要内容",
-  });
-};
-
-// 插入关联线
-const insertLine = () => {
-  mindMap.associativeLine.createLineFromActiveNode();
-};
-
-const initMindMap = () => {
-  mindMap = new MindMap({
-    el: document.getElementById("mindMapContainer"),
-    data: {
-      data: {
-        text: "根节点",
-      },
-      children: [
-        {
-          data: {
-            text: "二级节点",
-          },
-          children: [],
-        },
-        {
-          data: {
-            text: "二级节点",
-          },
-          children: [],
-        },
-      ],
-    },
-    initRootNodePosition: ["left", "center"],
-  });
-
-  // 监听节点激活事件
-  mindMap.on("node_active", (node: any, nodeList: never[]) => {
-    activeNodes.value = nodeList;
-  });
-
-  // 前进回退事件
-  mindMap.on("back_forward", (index: number, len: number) => {
-    isStart.value = index <= 0;
-    isEnd.value = index >= len - 1;
-  });
-};
-
-onMounted(() => {
-  initMindMap();
 });
 </script>
 
 <style lang="scss" scoped>
 .mind-map-feature-container {
   height: 100%;
-  padding: 20px;
-}
-
-.mind-map-container {
-  position: relative;
-  width: 100%;
-  height: 100%;
-
-  .toolbar {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-  }
 }
 </style>
